f00f8424d0c4cb933eacdc2fdde0fab61c8a9cdc
Symasia <ryftcore@gmail.com>
2021-02-11 19:37:54 +0530

    feat: add fade in transition to sections in every page

--- pages/company-info.js
+++ pages/company-info.js
@@ -1,61 +1,64 @@
 import Layout from '../components/layout';
 import styles from '../styles/companyInfo.module.css';
+import FadeInWhenVisible from '../components/fadeInWhenVisible';
 
 const CompanyInfoPage = () => {
   return (
     <Layout>
-      <section className='container'>
-        <h1>会社概要</h1>
-        <div className={styles.row}>
-          <img
-            height='150px'
-            width='260px'
-            src='assets/baryon_logo.png'
-            alt='baryon logo'
-          />
-          <table className={styles['company-info']}>
-            <thead>
-              <tr>
-                <th colSpan='1'></th>
-                <th colSpan='2'></th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>会社名</td>
-                <td>株式会社バリオンパートナーズ</td>
-              </tr>
-              <tr>
-                <td>業務内容</td>
-                <td>
-                  海外投資、新規事業立ち上げ等の助言、共同投資などを主たる業務とする。
-                </td>
-              </tr>
-              <tr>
-                <td>メールアドレス</td>
-                <td>info@bayon.jp</td>
-              </tr>
-              <tr>
-                <td>代表取締役</td>
-                <td>姫野泰光</td>
-              </tr>
-              <tr>
-                <td>代表略歴</td>
-                <td>
-                  大学卒業後、30年近く大企業から中小企業にいたる幅広い企業において新規事
-                  業の立ち上げに携わる。立ち上げ時の戦略立案と実行、メンバーの雇用、予算の
-                  獲得や資金調達、監督官庁の許認可取得、リスク管理等を実際に経験して、多く
-                  の事業を成功に導く。また新興国の事業投資やファイナンスにも多くの経験を持
-                  つ。世界銀行グループ多数国間投資保証機関の前東京事務所長。三井住友信託銀
-                  行、メリルリンチ日本証券、モルガンスタンレー証券、マークリッチ・アンド・
-                  カンパニー、日商岩井などでの勤務経験を持つ。ファイナンスや会計にも詳しい。
-                  広島大学理学部物理学科卒業
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </section>
+      <FadeInWhenVisible>
+        <section className='container'>
+          <h1>会社概要</h1>
+          <div className={styles.row}>
+            <img
+              height='150px'
+              width='260px'
+              src='assets/baryon_logo.png'
+              alt='baryon logo'
+            />
+            <table className={styles['company-info']}>
+              <thead>
+                <tr>
+                  <th colSpan='1'></th>
+                  <th colSpan='2'></th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>会社名</td>
+                  <td>株式会社バリオンパートナーズ</td>
+                </tr>
+                <tr>
+                  <td>業務内容</td>
+                  <td>
+                    海外投資、新規事業立ち上げ等の助言、共同投資などを主たる業務とする。
+                  </td>
+                </tr>
+                <tr>
+                  <td>メールアドレス</td>
+                  <td>info@bayon.jp</td>
+                </tr>
+                <tr>
+                  <td>代表取締役</td>
+                  <td>姫野泰光</td>
+                </tr>
+                <tr>
+                  <td>代表略歴</td>
+                  <td>
+                    大学卒業後、30年近く大企業から中小企業にいたる幅広い企業において新規事
+                    業の立ち上げに携わる。立ち上げ時の戦略立案と実行、メンバーの雇用、予算の
+                    獲得や資金調達、監督官庁の許認可取得、リスク管理等を実際に経験して、多く
+                    の事業を成功に導く。また新興国の事業投資やファイナンスにも多くの経験を持
+                    つ。世界銀行グループ多数国間投資保証機関の前東京事務所長。三井住友信託銀
+                    行、メリルリンチ日本証券、モルガンスタンレー証券、マークリッチ・アンド・
+                    カンパニー、日商岩井などでの勤務経験を持つ。ファイナンスや会計にも詳しい。
+                    広島大学理学部物理学科卒業
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </section>
+      </FadeInWhenVisible>
     </Layout>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -4,139 +4,148 @@ import Button from '../components/button';
 import { FaMedapps } from 'react-icons/fa';
 import Layout from '../components/layout';
 import Link from 'next/link';
+import FadeInWhenVisible from '../components/fadeInWhenVisible';
 
 const HomePage = () => {
   const [ref, inView] = useInView({ threshold: 0 });
 
   return (
     <Layout shouldDrawerTriggerBeVisible={!inView}>
-      <section id='top' ref={ref} className={styles.hero}>
-        <ul className={styles.navbar}>
-          <li>
-            <Link href='#top'>TOP</Link>
-          </li>
-          <li>
-            <Link href='/services'>業務内容</Link>
-          </li>
-          <li>
-            <Link href='/company-info'>会社概要</Link>
-          </li>
-          <li>
-            <Link href='/contact'>お問い合わせ</Link>
-          </li>
-        </ul>
-        <div className={styles.content}>
-          <img
-            className={styles['company-logo']}
-            height='50px'
-            width='80px'
-            src='assets/baryon_logo.png'
-            alt='baryon logo'
-          />
-          <h1>
-            未来のために、<span className='invert'>知恵を生かそう</span>
-          </h1>
-          <h2>新規事業・海外投資のプロ</h2>
-        </div>
-        <div className={styles.carousel}>
-          <img src='assets/hero-img.jpg' alt='busy city' />
-        </div>
-      </section>
-      <section className={styles['investment-section']}>
-        <h2 style={{ fontSize: '3.5rem', margin: 0 }}>
-          新規事業立ち上げと海外投資は
-        </h2>
-        <h3 style={{ fontSize: '2.5rem', margin: 0 }}>
-          バリオン・パートナーズへ
-        </h3>
-        <div className={styles['investment-section--content']}>
-          <img src='assets/hero-img.jpg' alt='busy road' />
-          <div>
-            <h4>
-              常に未来を見据えて、事業機会を探索し、
-              市場のニーズに応える事業立ち上げや投資 を行ってきたスキル
-            </h4>
-            <p>
-              法人・個人を問わず、いつもお客様のために考えるパー
-              トナーとして、気軽に相談できる相手として、いつも一
-              緒に歩む併走者でありたいと思っています。
-            </p>
-            <Button>私たちについて</Button>
+      <FadeInWhenVisible>
+        <section id='top' ref={ref} className={styles.hero}>
+          <ul className={styles.navbar}>
+            <li>
+              <Link href='#top'>TOP</Link>
+            </li>
+            <li>
+              <Link href='/services'>業務内容</Link>
+            </li>
+            <li>
+              <Link href='/company-info'>会社概要</Link>
+            </li>
+            <li>
+              <Link href='/contact'>お問い合わせ</Link>
+            </li>
+          </ul>
+          <div className={styles.content}>
+            <img
+              className={styles['company-logo']}
+              height='50px'
+              width='80px'
+              src='assets/baryon_logo.png'
+              alt='baryon logo'
+            />
+            <h1>
+              未来のために、<span className='invert'>知恵を生かそう</span>
+            </h1>
+            <h2>新規事業・海外投資のプロ</h2>
           </div>
-        </div>
-      </section>
-      <section className='section'>
-        <h2
-          style={{
-            margin: 0,
-            marginBottom: '2.825rem',
-            textAlign: 'center',
-          }}
-        >
-          新規事業を考えたい、海外に進出・投資をしたい
-          と思ったら、まずご相談ください
-        </h2>
-        <img
-          style={{ objectFit: 'cover', width: '90%' }}
-          src='assets/hero-img.jpg'
-          alt='busy road'
-        />
-      </section>
-      <section className={styles.services}>
-        <h2
-          style={{
-            margin: 0,
-            marginBottom: '2.825rem',
-            textAlign: 'center',
-          }}
-        >
-          バリオン・パートナーズのサービス内容の一例
-        </h2>
+          <div className={styles.carousel}>
+            <img src='assets/hero-img.jpg' alt='busy city' />
+          </div>
+        </section>
+      </FadeInWhenVisible>
+      <FadeInWhenVisible>
+        <section className={styles['investment-section']}>
+          <h2 style={{ fontSize: '3.5rem', margin: 0 }}>
+            新規事業立ち上げと海外投資は
+          </h2>
+          <h3 style={{ fontSize: '2.5rem', margin: 0 }}>
+            バリオン・パートナーズへ
+          </h3>
+          <div className={styles['investment-section--content']}>
+            <img src='assets/hero-img.jpg' alt='busy road' />
+            <div>
+              <h4>
+                常に未来を見据えて、事業機会を探索し、
+                市場のニーズに応える事業立ち上げや投資 を行ってきたスキル
+              </h4>
+              <p>
+                法人・個人を問わず、いつもお客様のために考えるパー
+                トナーとして、気軽に相談できる相手として、いつも一
+                緒に歩む併走者でありたいと思っています。
+              </p>
+              <Button>私たちについて</Button>
+            </div>
+          </div>
+        </section>
+      </FadeInWhenVisible>
+      <FadeInWhenVisible>
+        <section className='section'>
+          <h2
+            style={{
+              margin: 0,
+              marginBottom: '2.825rem',
+              textAlign: 'center',
+            }}
+          >
+            新規事業を考えたい、海外に進出・投資をしたい
+            と思ったら、まずご相談ください
+          </h2>
+          <img
+            style={{ objectFit: 'cover', width: '90%' }}
+            src='assets/hero-img.jpg'
+            alt='busy road'
+          />
+        </section>
+      </FadeInWhenVisible>
+      <FadeInWhenVisible>
+        <section className={styles.services}>
+          <h2
+            style={{
+              margin: 0,
+              marginBottom: '2.825rem',
+              textAlign: 'center',
+            }}
+          >
+            バリオン・パートナーズのサービス内容の一例
+          </h2>
 
-        <ul>
-          <li>
-            <FaMedapps />
-            <h4>海外投資支援</h4>
-          </li>
-          <li>
-            <FaMedapps />
-            <h4>貿易業務支援</h4>
-          </li>
-          <li>
-            <FaMedapps />
-            <h4>海外進出支援</h4>
-          </li>
-          <li>
-            <FaMedapps />
-            <h4>新規事業 立ち上げ支援</h4>
-          </li>
-          <li>
-            <FaMedapps />
-            <h4>貿易業務支援 デジタル化支援</h4>
-          </li>
-          <li>
-            <FaMedapps />
-            <h4>国内戸建住宅 建設助言</h4>
-          </li>
-          <li>
-            <FaMedapps />
-            <h4>海外移住助言</h4>
-          </li>
-          <li>
-            <FaMedapps />
-            <h4>農業支援</h4>
-          </li>
-          <li>
-            <FaMedapps />
-            <h4>不動産開発支援</h4>
-          </li>
-          <li>
-            <FaMedapps />
-            <h4>ネット営業支援</h4>
-          </li>
-        </ul>
-        <Button>サービスについて</Button>
-      </section>
+          <ul>
+            <li>
+              <FaMedapps />
+              <h4>海外投資支援</h4>
+            </li>
+            <li>
+              <FaMedapps />
+              <h4>貿易業務支援</h4>
+            </li>
+            <li>
+              <FaMedapps />
+              <h4>海外進出支援</h4>
+            </li>
+            <li>
+              <FaMedapps />
+              <h4>新規事業 立ち上げ支援</h4>
+            </li>
+            <li>
+              <FaMedapps />
+              <h4>貿易業務支援 デジタル化支援</h4>
+            </li>
+            <li>
+              <FaMedapps />
+              <h4>国内戸建住宅 建設助言</h4>
+            </li>
+            <li>
+              <FaMedapps />
+              <h4>海外移住助言</h4>
+            </li>
+            <li>
+              <FaMedapps />
+              <h4>農業支援</h4>
+            </li>
+            <li>
+              <FaMedapps />
+              <h4>不動産開発支援</h4>
+            </li>
+            <li>
+              <FaMedapps />
+              <h4>ネット営業支援</h4>
+            </li>
+          </ul>
+          <Button>サービスについて</Button>
+        </section>
+      </FadeInWhenVisible>
     </Layout>
   );
 };
